2df58f336c1b623c55b44e181746cc6e0e15a0cc
Sanjeevkumar Sharma <sanjeev1996s@gmail.com>
2019-03-02 02:47:01 +0530

    Added style to login forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   // authenticated: Boolean;
