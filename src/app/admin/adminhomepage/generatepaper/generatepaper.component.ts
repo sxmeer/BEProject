fdf33d72cedd9443a21348db073f91a4e3d0baf0
Sanjeevkumar Sharma <sanjeev1996s@gmail.com>
2019-03-16 21:33:58 +0530

    Generate Paper total marks validation

--- src/app/admin/adminhomepage/generatepaper/generatepaper.component.ts
+++ src/app/admin/adminhomepage/generatepaper/generatepaper.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { AdminloginserviceService } from '../../adminloginservice.service';
-import { Models } from '../../models.model';
-import { Department } from '../../department.model';
-import { Semester } from '../../semesters.model';
-import { Subjects } from '../../subjects.model';
+import { Component, OnInit } from "@angular/core";
+import { AdminloginserviceService } from "../../adminloginservice.service";
+import { Models } from "../../models.model";
+import { Department } from "../../department.model";
+import { Semester } from "../../semesters.model";
+import { Subjects } from "../../subjects.model";
 
 @Component({
-  selector: 'app-generatepaper',
-  templateUrl: './generatepaper.component.html',
-  styleUrls: ['./generatepaper.component.css']
+  selector: "app-generatepaper",
+  templateUrl: "./generatepaper.component.html",
+  styleUrls: ["./generatepaper.component.css"]
 })
 export class GeneratepaperComponent implements OnInit {
-
   marksList = [];
   models: Models[];
-  modelDesc: Models[];
+  modelDesc: Models;
   questionDesc;
   public paperSpecification = {
     modelID: '',
@@ -31,7 +30,7 @@ export class GeneratepaperComponent implements OnInit {
   public semesters: Semester[];
   public subjects: Subjects[];
 
-  constructor(private adminLoginService: AdminloginserviceService) { }
+  constructor(private adminLoginService: AdminloginserviceService) {}
 
   ngOnInit() {
     this.adminLoginService.getModels();
@@ -39,7 +38,7 @@ export class GeneratepaperComponent implements OnInit {
       this.models = data;
     });
     this.adminLoginService.fetchDepartments();
-    this.adminLoginService.departments.subscribe((data: Department[])=> {
+    this.adminLoginService.departments.subscribe((data: Department[]) => {
       this.depts = data;
     });
     this.adminLoginService.semesters.subscribe((data: Semester[]) => {
@@ -48,13 +47,21 @@ export class GeneratepaperComponent implements OnInit {
     this.adminLoginService.subjects.subscribe((data: Subjects[]) => {
       this.subjects = data;
     });
-    this.adminLoginService.subjectData
-    .subscribe((data: {dept: number, marksType: Array<number>, moduleDetails: Array<string>, numberOfModules: number, sem: number, subId: number}) => {
-      this.paperSpecification.numberOfModules = data.numberOfModules;
-      // console.log(this.numberOfModules);
-      this.marksList = new Array(this.paperSpecification.numberOfModules);
-      this.addUnitwiseDistribution();
-    });
+    this.adminLoginService.subjectData.subscribe(
+      (data: {
+        dept: number;
+        marksType: Array<number>;
+        moduleDetails: Array<string>;
+        numberOfModules: number;
+        sem: number;
+        subId: number;
+      }) => {
+        this.paperSpecification.numberOfModules = data.numberOfModules;
+        // console.log(this.numberOfModules);
+        this.marksList = new Array(this.paperSpecification.numberOfModules);
+        this.addUnitwiseDistribution();
+      }
+    );
     this.paperSpecification.difficulty = new Array(3);
     this.paperSpecification.cognitive = new Array(3);
   }
@@ -63,14 +70,21 @@ export class GeneratepaperComponent implements OnInit {
     this.adminLoginService.fetchSemesters(this.paperSpecification.deptID);
   }
   onSemesterSelected() {
-    this.adminLoginService.fetchSubjects( this.paperSpecification.deptID, this.paperSpecification.semNo);
+    this.adminLoginService.fetchSubjects(
+      this.paperSpecification.deptID,
+      this.paperSpecification.semNo
+    );
   }
 
   onModelSelected() {
-    this.modelDesc = this.models.filter((item) => {
+    this.modelDesc = this.models.filter(item => {
       return item._id === this.paperSpecification.modelID;
-    });
-    this.questionDesc = JSON.stringify(this.modelDesc[0].questionModelList, undefined, 2);
+    })[0];
+    this.questionDesc = JSON.stringify(
+      this.modelDesc.questionModelList,
+      undefined,
+      2
+    );
   }
 
   onSubjectSelected() {
@@ -78,15 +92,57 @@ export class GeneratepaperComponent implements OnInit {
   }
 
   addUnitwiseDistribution() {
-    this.paperSpecification.unitwiseDistribution = new Array(this.paperSpecification.numberOfModules);
-    for(let i = 1; i <= this.paperSpecification.numberOfModules; i++) {
+    this.paperSpecification.unitwiseDistribution = new Array(
+      this.paperSpecification.numberOfModules
+    );
+    for (let i = 1; i <= this.paperSpecification.numberOfModules; i++) {
       this.marksList[i - 1] = i;
     }
-    console.log(this.marksList);
+    // console.log(this.marksList);
   }
 
   onSubmit() {
+    // validating marks
+    let sumOfMarks = 0;
+    for (let i = 0; i < this.paperSpecification.numberOfModules; i++) {
+      sumOfMarks += this.paperSpecification.unitwiseDistribution[i];
+    }
+    if (sumOfMarks !== this.modelDesc.totalMarks) {
+      document.getElementById("marksError").innerText =
+        "Sum of marks of Unitwise distribution must be equal to " +
+        this.modelDesc.totalMarks;
+        this.clearFunction();
+      return;
+    }
+    sumOfMarks = 0;
+    for (let i = 0; i < this.paperSpecification.difficulty.length; i++) {
+      sumOfMarks += this.paperSpecification.difficulty[i];
+    }
+    if (sumOfMarks !== this.modelDesc.totalMarks) {
+      document.getElementById("diffError").innerText =
+        "Sum of marks of difficulty level must be equal to " +
+        this.modelDesc.totalMarks;
+        this.clearFunction();
+      return;
+    }
+    sumOfMarks = 0;
+    for (let i = 0; i < this.paperSpecification.cognitive.length; i++) {
+      sumOfMarks += this.paperSpecification.cognitive[i];
+    }
+    if (sumOfMarks !== this.modelDesc.totalMarks) {
+      document.getElementById("cogError").innerText =
+        "Sum of marks of congitive level must be equal to " +
+        this.modelDesc.totalMarks;
+        this.clearFunction();
+      return;
+    }
     console.log(this.paperSpecification);
   }
-
+  clearFunction(){
+    setTimeout(() => {
+      document.getElementById("marksError").innerText = "";
+      document.getElementById("diffError").innerText = "";
+      document.getElementById("cogError").innerText = "";
+    }, 3000);
+  }
 }
